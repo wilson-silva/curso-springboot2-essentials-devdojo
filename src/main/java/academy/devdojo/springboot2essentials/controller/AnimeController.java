ebd32e58829efc47957fb155274ef606760d2d59
wilson-siva <wilson-gs@hotmail.com>
2023-02-23 14:13:23 -0300

    criado projeto

---
+++ src/main/java/academy/devdojo/springboot2essentials/controller/AnimeController.java
@@ -0,0 +1,27 @@
+package academy.devdojo.springboot2essentials.controller;
+
+import academy.devdojo.springboot2essentials.domain.Anime;
+import academy.devdojo.springboot2essentials.util.DateUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("anime")
+@Log4j2
+@RequiredArgsConstructor
+public class AnimeController {
+
+    private final DateUtil dateUtil;
+
+    @GetMapping("/list")
+    public List<Anime> list(){
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return List.of(new Anime("Boku no Hero"), new Anime("Berserk"));
+    }
+}
---
+++ src/main/java/academy/devdojo/springboot2essentials/domain/Anime.java
@@ -0,0 +1,14 @@
+package academy.devdojo.springboot2essentials.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Anime {
+
+    private String nome;
+
+}
